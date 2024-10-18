7ff5c0e6c3a80fcb364f895cb8cee1f1f47bda65
Grant Luo <hongzhiluo@Tnargs-Mac.local>
2024-10-17 18:55:02 -0700

    10-minute update

--- main.cpp
+++ main.cpp
@@ -8,10 +8,38 @@ class DoublyLinkedList // Declares the DoublyLinkedList class
     private:
         struct Node
         {
-            int data;
-            Node* prev;
-            Node* next;
-            // Initialization list to
+            int data; // The data in a Node
+            Node* prev; // The pointer to the previous Node
+            Node* next; // The pointer to the next Node
+            // Parametrized constructor that initializes the members using an initializer list
             Node(int val, Node* p = nullptr, Node* n = nullptr) : data(val), prev(p), next(n) {}
         };
+
+        Node* head; // The pointer to the first Node struct in the linked-list
+        Node* tail; // The pointer to the last Node struct in the linked-list
+
+    public:
+        // Default constructor to initialize an empty linked list
+        DoublyLinkedList() : head(nullptr), tail(nullptr) {}
+
+        void insert_after(int value, int position)
+        {
+            if (position < 0)
+            {
+                cout << "Position must be >= 0." << endl;
+                return;
+            }
+
+            Node* newNode = new Node(value);
+            if (!head)
+            {
+                head = tail = newNode;
+                return;
+            }
+
+            // Traversing until the desired position is reached
+            Node* temp = head;
+            for (int i = 0; i < position && temp; ++i)
+                temp = temp->next;
+        }
 };
